4dd8956f83ac50340711c918de0f4ffc10afb500
Kaljuk <erik.kaljumae@gmail.com>
2018-10-21 15:16:13 +0300

    Bigger font for the answers

--- edutechApp/components/screens/QuestionScreen.js
+++ edutechApp/components/screens/QuestionScreen.js
@@ -215,7 +215,7 @@ const styles = StyleSheet.create({
         color: 'white',
         textAlign : 'center',
         fontWeight: 'bold',
-        fontSize: 18
+        fontSize: 24
     },
     questionCancelBox: {
         position: "absolute",
